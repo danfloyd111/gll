6a40932f04cc787014ec019063bff56b0eff57c3
Daniele Paolini <dan.floyd111@gmail.com>
2016-04-23 23:19:31 +0200

    Added the initial version of the constructor

--- gll.c
+++ gll.c
@@ -1,2 +1,16 @@
-#include <stdlib.h>
-#include <string.h>
+#include "gll.h"
+
+/* Please see gll.h for further informations */
+
+/* Create new list */
+list_t* list() {
+  list_t* this;
+  if(!(this = malloc(sizeof(list_t)))) {
+    errno = ENOMEM;
+    return NULL;
+  }
+  this->head = NULL;
+  this->tail = NULL;
+  this->length = 0;
+  return this;
+}
--- gll.h
+++ gll.h
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
+/* DATA STRUCTURES */
+ 
 /* Element of the list */
 typedef struct _node_t {
   void* data;
@@ -11,10 +14,17 @@ typedef struct _node_t {
 /* List structure */
 typedef struct _list_t {
   node* head;
-  node* tail;
+  node* tail; /* maybe is not necessary */
 
   int length;
-  int nodeSize; /* this is not necessary - the size is fixed*/
 } list_t;
 
 /* FUNCTIONS */
+
+/*
+  DESCRIPTION: Creates a new empty list
+  ARGUMENTS: none
+  RETURNS the pointer to the newly created (empty) list
+  ON ERROR: returns null and sets errno to ENOMEM
+*/
+list_t* list();
