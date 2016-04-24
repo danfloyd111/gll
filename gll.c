e829113d4eaa9f9fe961837705f8d64c63c11b70
Daniele Paolini <dan.floyd111@gmail.com>
2016-04-24 22:11:01 +0200

    Added push function - testing and comment checking needed

--- gll.c
+++ gll.c
@@ -3,7 +3,7 @@
 /* Please see gll.h for further informations */
 
 /* Create new list */
-list_t* list() {
+list_t* list () {
   list_t* this;
   if(!(this = malloc(sizeof(list_t)))) {
     errno = ENOMEM;
@@ -13,4 +13,23 @@ list_t* list() {
   this->tail = NULL;
   this->length = 0;
   return this;
+}
+
+/* Push new chunk of data in the head of the list */
+int push (void* dat, list_t* lis) {
+  if(dat == NULL || lis == NULL) {
+    errno = EINVAL;
+    return 1;
+  }
+  node_t* this;
+  if(!(this = malloc(sizeof(node_t)))) {
+    errno = ENOMEM;
+    return 1;
+  }
+  this->data = dat;
+  this->prev = NULL;
+  this->next = lis->head;
+  lis->head = this;
+  lis->length ++;
+  return 0;
 }
--- gll.h
+++ gll.h
@@ -25,6 +25,18 @@ typedef struct _list_t {
   DESCRIPTION: Creates a new empty list
   ARGUMENTS: none
   RETURNS the pointer to the newly created (empty) list
-  ON ERROR: returns null and sets errno to ENOMEM
+  ON ERROR: returns null and sets errno to ENOMEM if memory error
+            occours
 */
-list_t* list();
+list_t* list ();
+
+/*
+  DESCRIPTION: Inserts the chunk of data in the head of the list
+  ARGUMENTS:
+    dat - pointer to the chunk of data to insert
+    lis - pointer to the list
+  RETURNS 0 if the function terminates correctly
+  ON ERROR: returns 1 and sets errno to ENOMEM if memory error
+            occours / EINVAL if one of the arguments is null
+*/
+int push (void*, list_t*);
