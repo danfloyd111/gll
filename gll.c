95606b380d45341286a25243fbdc725e5fa4ac1d
danfloyd111 <dan.floyd111@gmail.com>
2016-04-27 20:47:14 +0200

    Added index_of and some prototypes

--- gll.c
+++ gll.c
@@ -4,8 +4,12 @@
 /* IMPLEMENTATION */
 
 /* Create new list */
-list_t* list () {
+list_t* list (int(*comp)(void*,void*)) {
   list_t* this;
+  if(comp == NULL) {
+    errno = EINVAL;
+    return NULL;
+  }
   if(!(this = malloc(sizeof(list_t)))) {
     errno = ENOMEM;
     return NULL;
@@ -13,6 +17,7 @@ list_t* list () {
   this->head = NULL;
   this->tail = NULL;
   this->length = 0;
+  this->compare = comp;
   return this;
 }
 
@@ -75,16 +80,92 @@ void* pop (list_t* lis) {
       errno = ENODATA;
     else {
       f = lis->head;
-      e = f->data;
+      /* data cannot be null -> see gll.h */
+      e = f->data; 
       if(length(lis)==1)
-	tail = NULL;
+	lis->tail = NULL;
       else
 	f->next->prev = NULL;
       lis->head = f->next;
-      free(f);
+      /* I decided to not check memory errors on free */
+      free(f); 
+      lis->length --;
     }
   } else {
     errno = EINVAL;
   }
   return e;
+}
+
+/* Return and remove the last element from the list */
+void* cut (list_t* lis) {
+  node_t* l = NULL;
+  void* e = NULL;
+  if(lis) {
+    if(!length(lis))
+      errno = ENODATA;
+    else {
+      l = lis->tail;
+      /* data cannot be null -> see gll.h */
+      e = l->data;
+      if(length(lis)==1)
+	lis->head = NULL;
+      else
+	l->prev->next = NULL;
+      lis->tail = l->prev;
+      /* I decided to not check memory errors on free  */
+      free(l);
+      lis->length --;
+    }
+  } else {
+    errno = EINVAL;
+  }
+  return e;
+}
+
+/* Remove an element from the list */
+int remove (list_t* lis, int ind) {
+  if(!length(lis)) {
+    errno = ENODATA;
+    return 1;
+  }
+  if(ind < 0 || ind >= length(lis)) {
+    errno = EINVAL;
+    return 1;
+  }
+  node_t* it = lis->head;
+  while (ind) {
+    it = it->next;
+    ind--;
+  }
+  free(it->data);
+  /* list merging */
+  if(it->prev)
+    it->prev->next = it->next;
+  if(it->next)
+    it->next->prev = it->prev;
+  /* head or tail removal */
+  if(ind == 0)
+    lis->head = it->next;
+  if(ind == (length(lis)-1))
+    lis->tail = it->prev;
+  list->length --;
+  free(it);
+  return 0;
+}
+
+/* Returns the index of the element */
+int index_of (list_t* lis, void* dat) {
+  int ind = -1;
+  if(!lis || !dat) {
+    errno = EINVAL;
+    return ind;
+  }
+  int len = length(lis);
+  if(len) {
+    node_t* it = lis->head;
+    for(int j=0; j<len; j++) {
+      /* NEED GET & SET FUNCTIONS HERE */
+    }
+  }
 }
--- gll.h
+++ gll.h
@@ -16,18 +16,21 @@ typedef struct _list_t {
   node* head;
   node* tail;
   int length;
+  int(*compare)(void*,void*); /*used to compare two data elements*/
 } list_t;
 
 /* FUNCTIONS */
 
 /*
   DESCRIPTION: Creates a new empty list
-  ARGUMENTS: none
+  ARGUMENTS:
+    comp - int function that takes two arguments
+           used to compare two elements
   RETURNS the pointer to the newly created (empty) list
   ON ERROR: returns null and sets errno to ENOMEM if memory error
-            occours
+            occours / EINVAL if one of the arguments is null
 */
-list_t* list ();
+list_t* list (int(*comp)(void*,void*));
 
 /*
   DESCRIPTION: Returns the length of the list passed as argument
@@ -73,3 +76,51 @@ int append (void*, list_t*);
             is null / ENODATA if the list is empty
 */
 void* pop (list_t*);
+
+/*
+  DESCRIPTION: Returns and remove the last element from the list
+               passed as argument
+  ARGUMENTS:
+    lis - pointer to the list
+  RETURNS the last element of the list
+  ON ERROR: returns NULL and sets errno to EINVAL if the argument
+            is null / ENODATA if the list is empty
+*/
+void* cut (list_t*);
+
+/*
+  DESCRIPTION: Removes the element at the position passed as
+               argument from the list passed as argument
+  ARGUMENTS:
+    lis - pointer to the list
+    ind - index of the element
+  RETURNS 0 if the function ends correctly
+  ON ERROR: returns 1 and sets errno to EINVAL if one of the
+            arguments is null or -1 < ind < length(lis)
+	    n.b. indexes start from 0 / ENODATA if the list
+	    is empty
+*/
+int remove (list_t*, int);
+
+/*
+  DESCRIPTION: Returns the index of the element passed as argument
+               if is contained in the list passed as argument
+  ARGUMENTS:
+    lis - pointer to the list
+    dat - pointer to the element
+  RETURNS the index of the element (count starts from 0)
+  ON ERROR: returns -1 and sets errno to ENODATA if the element is 
+            not contained in the list or if the list is empty /
+            EINVAL if one of the arguments is null
+	    n.b. this function perform a deep test, so it will compare
+            the data itself, not the address
+*/
+int index_of (list_t*, void*);
+
+/*
+  DESCRIPTION: Returns the element at the index passed as argument
+ */
+void* get (list_t*, int);
+
+/* NEED DOCS HERE */
+int set (list_t*, int);
